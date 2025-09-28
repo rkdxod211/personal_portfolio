ab77f9491945ff1e932df38cb684867c5731b897
rkdxod211 <bdkh5102@gmail.com>
2025-09-28 17:24:11 -0500

    .

---
+++ script.js
@@ -0,0 +1,16 @@
+const webButton = document.getElementById('webButton');
+webButton.addEventListener('click', () => {
+    window.location.href = "about.html"; 
+});
+
+const appButton = document.getElementById('appButton');
+appButton.addEventListener('click', () => {
+    window.location.href = "contact.html"; 
+});
+
+const etcButton = document.getElementById('etcButton');
+etcButton.addEventListener('click', () => {
+    window.location.href = "index.html"; 
+});
+
+
